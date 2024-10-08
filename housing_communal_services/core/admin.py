466ccc77ec8628a44c82d6a1b4259e0de50ece1b
StackCanary <danizmd8@gmail.com>
2024-10-08 18:15:52 +0300

    feat: add core models

--- housing_communal_services/core/admin.py
+++ housing_communal_services/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.site_header = "Администрирование ЖКХ"
+
+admin.site.register(House)
+admin.site.register(Flat)
+admin.site.register(TariffType)
+admin.site.register(Tariff)
+admin.site.register(Meter)
+admin.site.register(MeterReading)
--- housing_communal_services/core/models.py
+++ housing_communal_services/core/models.py
@@ -1,3 +1,82 @@
+from decimal import Decimal
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from datetime import datetime
+
+from django.forms import ValidationError
+
 # Create your models here.
+
+class House(models.Model):
+    """Модель дома"""
+    address = models.CharField(verbose_name="Адрес", max_length=256, unique=True)
+    area = models.DecimalField(verbose_name="Площадь", decimal_places=2, max_digits=16, validators=[MinValueValidator(Decimal("0.01"))])
+    
+    def __str__(self) -> str:
+        return f"Дом: {self.address}"
+    
+    class Meta:
+        verbose_name = "Дом"
+        verbose_name_plural = "Дома"
+
+
+class Flat(models.Model):
+    """Модель квартиры"""
+    # используем CharField, так как могут быть специфичные номера квартир (110/1 или 110A и подобные)
+    number = models.CharField(verbose_name="Номер", max_length=128, unique=True)
+    house = models.ForeignKey(House, related_name="flats", on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.house}; Квартира {self.number}"
+    
+    class Meta:
+        verbose_name = "Квартира"
+        verbose_name_plural = "Квартиры"
+
+
+class TariffType(models.Model):
+    """Типы тарифов"""
+    name = models.CharField(verbose_name="Название типа тарифа", max_length=128, unique=True)
+
+    class Meta:
+        verbose_name = "Тип тарифа"
+        verbose_name_plural = "Типы тарифов"
+
+
+class Tariff(models.Model):
+    """Модель тарифа"""
+    price_per_unit = models.DecimalField(verbose_name="Цена за единицу", decimal_places=2, max_digits=16, validators=[MinValueValidator(Decimal("0.01"))])
+    tariff_type = models.ForeignKey(TariffType, related_name="tariffs", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Тариф"
+        verbose_name_plural = "Тарифы"
+
+
+class Meter(models.Model):
+    """Модель счётчика в квартире"""
+    flat = models.ForeignKey(Flat, related_name="meters", on_delete=models.CASCADE)
+    tariff = models.ForeignKey(Tariff, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Счётчик"
+        verbose_name_plural = "Счётчики"
+
+
+class MeterReading(models.Model):
+    month = models.IntegerField(verbose_name="Месяц", validators=[MinValueValidator(1), MaxValueValidator(12)])
+    year = models.IntegerField(verbose_name="Год")
+    meter = models.ForeignKey(Meter, related_name="readings", on_delete=models.CASCADE)
+    
+    def clean(self) -> None:
+        current_date = datetime.now()
+        
+        meter_reading_date = datetime(year=self.year, month=self.month)
+        
+        if meter_reading_date > current_date:
+            raise ValidationError("Дата показания не может быть больше текущего месяца")
+
+    class Meta:
+        verbose_name = "Показания счётчика"
+        verbose_name_plural = "Показания счётчиков"
