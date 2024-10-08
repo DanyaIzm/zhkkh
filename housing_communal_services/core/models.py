98fa6a17a1b45d090ec2541e20c2253b1fdf0060
StackCanary <danizmd8@gmail.com>
2024-10-08 18:38:29 +0300

    fix: add __str__ methods to all models in core

--- housing_communal_services/core/models.py
+++ housing_communal_services/core/models.py
@@ -25,7 +25,7 @@ class Flat(models.Model):
     """Модель квартиры"""
     # используем CharField, так как могут быть специфичные номера квартир (110/1 или 110A и подобные)
     number = models.CharField(verbose_name="Номер", max_length=128, unique=True)
-    house = models.ForeignKey(House, related_name="flats", on_delete=models.CASCADE)
+    house = models.ForeignKey(House, verbose_name="Дом", related_name="flats", on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return f"{self.house}; Квартира {self.number}"
@@ -39,6 +39,9 @@ class TariffType(models.Model):
     """Типы тарифов"""
     name = models.CharField(verbose_name="Название типа тарифа", max_length=128, unique=True)
 
+    def __str__(self) -> str:
+        return f"Тип тарифа: {self.name}"
+
     class Meta:
         verbose_name = "Тип тарифа"
         verbose_name_plural = "Типы тарифов"
@@ -47,7 +50,10 @@ class TariffType(models.Model):
 class Tariff(models.Model):
     """Модель тарифа"""
     price_per_unit = models.DecimalField(verbose_name="Цена за единицу", decimal_places=2, max_digits=16, validators=[MinValueValidator(Decimal("0.01"))])
-    tariff_type = models.ForeignKey(TariffType, related_name="tariffs", on_delete=models.CASCADE)
+    tariff_type = models.ForeignKey(TariffType, verbose_name="Тип тарифа", related_name="tariffs", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Тариф: {self.tariff_type.name} за {self.price_per_unit}"
 
     class Meta:
         verbose_name = "Тариф"
@@ -56,8 +62,11 @@ class Tariff(models.Model):
 
 class Meter(models.Model):
     """Модель счётчика в квартире"""
-    flat = models.ForeignKey(Flat, related_name="meters", on_delete=models.CASCADE)
-    tariff = models.ForeignKey(Tariff, on_delete=models.CASCADE)
+    flat = models.ForeignKey(Flat, verbose_name="Квартира", related_name="meters", on_delete=models.CASCADE)
+    tariff = models.ForeignKey(Tariff, verbose_name="Тариф", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Счётчик {self.tariff.tarrif_type}({self.tariff.price_per_unit}) в {self.flat}"
 
     class Meta:
         verbose_name = "Счётчик"
@@ -67,7 +76,10 @@ class Meter(models.Model):
 class MeterReading(models.Model):
     month = models.IntegerField(verbose_name="Месяц", validators=[MinValueValidator(1), MaxValueValidator(12)])
     year = models.IntegerField(verbose_name="Год")
-    meter = models.ForeignKey(Meter, related_name="readings", on_delete=models.CASCADE)
+    meter = models.ForeignKey(Meter, verbose_name="Счётчик", related_name="readings", on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.month}.{self.year}; {self.meter}"
     
     def clean(self) -> None:
         current_date = datetime.now()
