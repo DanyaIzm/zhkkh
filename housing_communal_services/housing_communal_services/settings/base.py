9e083c6b19af83ce6e82ab0079a2c9915ef9b370
StackCanary <danizmd8@gmail.com>
2024-10-13 14:50:14 +0300

    build: add docker compose and production build

--- housing_communal_services/housing_communal_services/settings/base.py
+++ housing_communal_services/housing_communal_services/settings/base.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,4 +141,4 @@ SPECTACULAR_SETTINGS = {
 }
 
 
-CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379")
